e09ef46401865f5c07882af319bb70b6122934e6
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-03 11:35:03 +0100

    Day 3: add data

--- day3/data_example.js
+++ day3/data_example.js
@@ -1,1 +1,1 @@
-export const data_example = 'forward 5,down 5,forward 8,up 3,down 8,forward 2';
+export const data_example = '00100,11110,10110,10111,10101,01111,00111,11100,10000,11001,00010,01010';
