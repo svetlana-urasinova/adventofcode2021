9ed2d3f416080fa6339672360b32f42dd05878f3
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-03 11:40:06 +0100

    Day 3: add test

--- day3/app.js
+++ day3/app.js
@@ -1,39 +1,14 @@
 import { data } from "./data.js";
 
-const parsePath = (str) => {
-    const [direction, lng] = str.split(' ');
-    return {direction, lng: Number(lng)};
+const prepareData = data => data.split(',').map(el => el.trim());
+
+const generate = data => {
+    console.log(data);
+    
 }
-
-const findPosition = (data) => {
-    const res = data.split(',').reduce((acc, el) => {
-        const path = parsePath(el);
-        path.direction === 'forward' ? acc.x += path.lng
-            : acc.y += path.direction === 'down' ? path.lng : -1 * path.lng;
-        return acc;
-    }, {x: 0, y: 0});
-
-    return res.x * res.y;
-}
-
-const findAimedPosition = (data) => {
-    const res = data.split(',').reduce((acc, el) => {
-        const path = parsePath(el);
-        if (path.direction === 'forward') {
-            acc.x += path.lng;
-            acc.y += acc.aim * path.lng;
-        } else {
-            acc.aim += path.direction === 'down' ? path.lng : -1 * path.lng;
-        }
-        return acc;
-    }, {x: 0, y: 0, aim: 0});
-
-    return res.x * res.y;
-}
-
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = findAimedPosition(data);
+    document.querySelector('.app').innerHTML = generate(prepareData(data));
 }
 
-export { findPosition, findAimedPosition }; 
+export { generate }; 
