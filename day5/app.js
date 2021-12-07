8f6a19105853e9fc6bdc7435c62afefd647bd02b
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-07 19:14:39 +0100

    Day 5: initial commit

---
+++ day5/app.js
@@ -0,0 +1,12 @@
+import { data_example } from "./data_example.js";
+import { data } from "./data.js";
+
+const findOverlapPoints = (data) => {
+
+}
+
+window.onload = () => {
+    document.querySelector('.app').findOverlapPoints = '';
+}
+
+export { findOverlapPoints };
