8ff1c6fcf7e76da1c555e515fa13736d6aa1ec22
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-07 19:15:38 +0100

    Day 5: fix wrong assignment

--- day5/app.js
+++ day5/app.js
@@ -2,11 +2,11 @@ import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
 const findOverlapPoints = (data) => {
-
+    return '';
 }
 
 window.onload = () => {
-    document.querySelector('.app').findOverlapPoints = '';
+    document.querySelector('.app').innerHTML = findOverlapPoints(data);
 }
 
 export { findOverlapPoints };
