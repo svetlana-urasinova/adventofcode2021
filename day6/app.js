ea4242aa00b916318a3efcc0b3fa329b9df4851e
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-09 16:44:56 +0100

    Day 6: solved first part
    Day 6: solved first part

--- day6/app.js
+++ day6/app.js
@@ -1,12 +1,31 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
-const countLanternFish = (data) => {
+const countLanternFish = (data, defaultDuration) => {
+    const days = 80;
+    const startNewDay = data => {
+        const newborn = [];
+        for (let i in data) {
+            if (data[i] === 0) {
+                data[i] = defaultDuration - 1;
+                newborn.push(defaultDuration + 1);
+            } else {
+                data[i]--;
+            }
+        }
+        return [...data, ...newborn];
+    }
     
+    let school = data.split(',').map(el => +el);
+    for (let i = 1; i <= days; i++) {
+        school = startNewDay(school);
+    }
+
+    return school.length;
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = countLanternFish(data);
+    document.querySelector('.app').innerHTML = countLanternFish(data, 7);
 }
 
 export { countLanternFish };
