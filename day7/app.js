5630e498d62ef45b2cca6e10fbfa840f3a60eb33
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-09 19:07:22 +0100

    Day 7: solved part 1

--- day7/app.js
+++ day7/app.js
@@ -1,12 +1,16 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
-const calculateFuel = () => {
-    
+const calculateSum = data => data.reduce((acc, el) => +acc + +el);
+
+const calculateFuel = (data, inc=false) => {
+    const crabs = data.split(',').sort((a, b) => a - b);
+    const median = crabs[Math.floor(crabs.length / 2)]
+    return calculateSum(crabs.map(el => Math.abs(median - el)));
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = calculateFuel(data_example);
+    document.querySelector('.app').innerHTML = calculateFuel(data);
 }
 
 export { calculateFuel };
