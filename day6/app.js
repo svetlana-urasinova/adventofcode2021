bf687ed55926ab235154966bc99d44ffcc11a1c9
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-09 17:55:17 +0100

    Day 6: update solution and solve part 2

--- day6/app.js
+++ day6/app.js
@@ -1,31 +1,32 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
-const countLanternFish = (data, defaultDuration) => {
-    const days = 80;
+const countLanternFish = (data, days) => {
+    let fish = data.split(',').reduce((acc, el) => {
+        acc[el] = ++acc[el] || 1;
+        return acc;
+    }, {});
+
     const startNewDay = data => {
-        const newborn = [];
-        for (let i in data) {
-            if (data[i] === 0) {
-                data[i] = defaultDuration - 1;
-                newborn.push(defaultDuration + 1);
-            } else {
-                data[i]--;
-            }
+        const defaultIndex = 6;
+        const newbornIndex = 8;
+        let res = {};
+        res[defaultIndex] = 0;
+        res[newbornIndex] = 0;
+        for (let i of Object.keys(data)) {
+            if (+i !== 0) { res[i - 1] = data[i]; }
         }
-        return [...data, ...newborn];
-    }
-    
-    let school = data.split(',').map(el => +el);
-    for (let i = 1; i <= days; i++) {
-        school = startNewDay(school);
+        res[defaultIndex] += data[0] ?? 0;
+        res[newbornIndex] += data[0] ?? 0;
+        return res;
     }
 
-    return school.length;
+    for (let i = 1; i <= days; i++) { fish = startNewDay(fish); }
+    return Object.values(fish).reduce((acc, el) => +acc + +el);    
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = countLanternFish(data, 7);
+    document.querySelector('.app').innerHTML = countLanternFish(data, 256);
 }
 
 export { countLanternFish };
