3aa1390cd9f916eab4d952bd7e95cb446385c681
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-12 00:34:50 +0100

    Add another example

--- day11/app.js
+++ day11/app.js
@@ -1,15 +1,16 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
+import { data_example2 } from "./data_example2.js";
 
 const parseData = data => data.split(/\n\s+/);
 
-countFlashes = data => {
+const countFlashes = data => {
     const octopuses = parseData(data);
     return null;
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = countFlashes(data_example);
+    document.querySelector('.app').innerHTML = countFlashes(data_example2);
 }
 
 export { countFlashes };
---
+++ day11/data_example2.js
@@ -0,0 +1,5 @@
+export const data_example2 = `11111
+19991
+19191
+19991
+11111`;
