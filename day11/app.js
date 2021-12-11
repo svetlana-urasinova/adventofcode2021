062589ca036ecff65ea281fdd43ee608005f8be9
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-12 00:37:33 +0100

    Day 11: update data parsing

--- day11/app.js
+++ day11/app.js
@@ -2,10 +2,11 @@ import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 import { data_example2 } from "./data_example2.js";
 
-const parseData = data => data.split(/\n\s+/);
+const parseData = data => data.split(/\n/).map(el => el.split('').map(el => { return { energy: el, flashed: false }}));
 
 const countFlashes = data => {
     const octopuses = parseData(data);
+    console.log(octopuses);
     return null;
 }
 
