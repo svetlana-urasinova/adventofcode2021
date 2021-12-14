557a947309fd6ce2b9be37625f93bd92d2a835b2
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-14 17:38:05 +0100

    Day 1: add data parser

--- day13/app.js
+++ day13/app.js
@@ -1,14 +1,27 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
-const parseData = data => data.split(/\n/);
+const parseData = data => {
+    let [points, fold] = data.split(/\n\n/).map(el => el.split(/\n/));
+    points = points.map(el => {
+        const [x, y] = el.split(',').map(el => +el);
+        return {x, y};
+    });
+    fold = fold.map(el => {
+        let [coord, val] = el.replace('fold along ', '').split('=');
+        val = +val;
+        return {coord, val};
+    });
+    return {points, fold};
+}
 
 const countDots = data => {
     const manual = parseData(data);
+    console.log(manual);
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = countDots(data, true);
+    document.querySelector('.app').innerHTML = countDots(data_example);
 }
 
 export { countDots };
