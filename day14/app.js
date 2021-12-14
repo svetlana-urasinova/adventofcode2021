c4bfba8ecd4cc0e1655bff0ada0128e66db2cdae
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-14 22:35:28 +0100

    Day 14: solve part 2

--- day14/app.js
+++ day14/app.js
@@ -2,52 +2,73 @@ import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
 const parseData = data => {
-    let [ template, rulesArr ] = data.split(/\n\n/);
-    const rules = {};
-    rulesArr.split(/\n/).forEach(el => {
+    let [ template, rules ] = data.split(/\n\n/);
+    return { template, rules: parseRules(rules) };
+}
+
+const parseTemplate = template => {
+    const res = {};
+    for (let i = 0; i < template.length - 1; i++) {
+        const current = template[i];
+        const next = template[i + 1];
+        if (res[current] === undefined) { res[current] = {}};
+        res[current][next] = res[current][next] === undefined ? 1 : ++res[current][next];
+    }
+    return res;
+}
+
+const parseRules = rules => {
+    const res = {};
+    rules.split(/\n/).forEach(el => {
         const [ from, to ] = el.split(' -> ');
-        rules[from] = to;
+        res[from] = to;
     });
-    return { template, rules };
+    return res;
 }
 
 const grow = (polymer, rules) => {
-    let newPolymer = polymer[0];
-    for (let i = 1; i < polymer.length; i++) {
-        const str = polymer.slice(i - 1, i + 1);
-        if (rules[str] === undefined) {
-        } else {
-            newPolymer += `${rules[str]}${str[1]}`;
+    let res = {};
+    for (let current in polymer) {
+        for (let next in polymer[current]) {
+            const insert = rules[`${current}${next}`];
+            const count = polymer[current][next];
+            res[current] = res[current] || {};
+            res[current][insert] = res[current][insert] + count || count;
+            res[insert] = res[insert] || {};
+            res[insert][next] = res[insert][next] + count || count;
         }
     }
-    return newPolymer;
+    return res;
 }
 
 const getPolymer = (template, rules, steps) => {
-    let polymer = template;  
+    let polymer = parseTemplate(template);
     for (let i = 0; i < steps; i++) {
         polymer = grow(polymer, rules);
     }
     return polymer;
 }
 
-const getFrequency = polymer => {
-    const frequencies = polymer.split('').reduce((acc, el) => {
-        acc[el] = acc[el] ? ++acc[el] : 1;
-        return acc;
-    }, {});
-    return Object.entries(frequencies).sort((a, b) => a[1] - b[1]).map(arr => {return {el: arr[0], val: arr[1]}});
+const getFrequency = (polymer, template) => {
+    const frequencies = {}; 
+    Object.values(polymer).forEach(obj => { 
+        Object.keys(obj).forEach(key => {
+            frequencies[key] = frequencies[key] ? frequencies[key] + obj[key] : obj[key];
+        });
+    });
+    frequencies[template[0]]++;
+    return Object.entries(frequencies).sort((a, b) => a[1] - b[1]).map(el => { return {el: [el[0]], val: el[1]}});
 }
 
-const partOne = data => {
+const countDiff = (data, steps) => {
     const { template, rules } = parseData(data);
-    const polymer = getPolymer(template, rules, 10);
-    const frequencies = getFrequency(polymer);
+    const polymer = getPolymer(template, rules, steps);
+    const frequencies = getFrequency(polymer, template);
     return frequencies[frequencies.length - 1].val - frequencies[0].val;    
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = partOne(data);
+    document.querySelector('.app').innerHTML = countDiff(data, 40);
 }
 
-export { partOne };
+export { countDiff };
