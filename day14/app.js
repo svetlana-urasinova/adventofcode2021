8fa913f2e598f7caf76db770b4b6bdd5849109d7
svetlana-urasinova <urasinova.work@gmail.com>
2021-12-14 19:08:13 +0100

    Day 14: add data parser

--- day14/app.js
+++ day14/app.js
@@ -1,14 +1,23 @@
 import { data_example } from "./data_example.js";
 import { data } from "./data.js";
 
-const parseData = data => data.split(/\n/);
+const parseData = data => {
+    let [ template, rules ] = data.split(/\n\n/);
+    rules = rules.split(/\n/).map(el => {
+        const [ from, to ] = el.split(' -> ');
+        return { [from]: to };
+    });
+    return { template, rules };
+}
 
 const findMostCommonElement = data => {
     const { template, rules } = parseData(data);
+    console.log(template);
+    console.log(rules);
 }
 
 window.onload = () => {
-    document.querySelector('.app').innerHTML = findMostCommonElement(data);
+    document.querySelector('.app').innerHTML = findMostCommonElement(data_example);
 }
 
 export { findMostCommonElement };
